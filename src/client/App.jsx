58ed202271d96b9089dd1d99e7309aaac638539d
lhjd <hongjinlau@gmail.com>
2019-11-22 21:27:28 +0800

    Show card

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-import Form from './components/form/form';
+import Card from '../client/components/card/card';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Form />
-        Welcome.
+        Flash Cards
+        <Card />
       </div>
     );
   }
---
+++ src/client/components/card/card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './style.scss';
+
+
+class Card extends React.Component {
+
+    render() {
+        return (
+            <div className={styles.card}>Card</div>
+        );
+
+
+    }
+
+}
+
+export default Card;
