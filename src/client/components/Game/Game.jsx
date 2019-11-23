bb44ece889816d43c920c5a9dadbc3ab15009fcd
kevinngth <kevin.ng.th@gmail.com>
2019-11-23 18:17:59 +0800

    bug fix

--- src/client/components/Game/Game.jsx
+++ src/client/components/Game/Game.jsx
@@ -7,7 +7,12 @@ class Game extends React.Component {
         super();
         this.state = {
             questionNum: 0,
-            showQuestion: true
+            showQuestion: true,
+            cards: [
+                {question: '2+2',answer: 4},
+                {question: 'capital of Malaysia',answer: 'Kuala Lumpur'},
+                {question: 'Kilometers in a mile',answer: 1.6},
+            ]
         };
     }
 
@@ -23,22 +28,13 @@ class Game extends React.Component {
         this.setState({showQuestion: !this.state.showQuestion})
     }
 
-    questionChoice(index) {
-        const cards = [
-            {question: '2+2',answer: 4},
-            {question: 'capital of Malaysia',answer: 'Kuala Lumpur'},
-            {question: 'Kilometers in a mile',answer: 1.6},
-        ]
-        return cards[index]
-    }
-
     render() {
         return (<div className={styles.game}>
             <div className={styles.section}>
                 <button onClick={()=>{this.previousQuestion()}}>Previous</button>
             </div>
             <div className={styles.section}>
-                <Card state={this.state} card={this.questionChoice(this.state.questionNum)} flipCard={()=>{this.flipCard()}}/>
+                <Card state={this.state} card={this.state.cards[this.state.questionNum]} flipCard={()=>{this.flipCard()}}/>
             </div>
             <div className={styles.section}>
                 <button onClick={()=>{this.nextQuestion()}}>Next</button>
