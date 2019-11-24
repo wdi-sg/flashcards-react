cace55fe7657e2238498f8e4a6beadd49330e98f
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-24 16:54:47 +0800

    Added to show/hide question and answer

--- src/client/components/form/form.jsx
+++ src/client/components/form/form.jsx
@@ -12,30 +12,72 @@ class Form extends React.Component {
     super();
 
     this.state = {
-      clicked:false
+        cards: [
+          {
+            question: '2+2',
+            answer: 4
+          },
+          {
+            question: 'capital of Malaysia',
+            answer: 'Kuala Lumpur'
+          },
+          {
+            question: 'Kilometers in a mile',
+            answer: 1.6
+          },
+        ],
+        index: 0,
+        showQuestion: false,
+        showAnswer: false
     };
   }
 
-  clickHandler(){
+  clickNext() {
+    console.log("Next Question is clicked");
 
-    this.setState({clicked:!this.state.clicked})
+    if(this.state.index === this.state.cards.length - 1) {
+        console.log("Last question lo!");
+    } else {
+        this.setState({index: this.state.index + 1, showAnswer: false});
+    }
+  }
+
+  clickAnswer() {
+    console.log("Show Answer is clicked");
+
+    // var currentIndex = this.state.index;
+    // console.log("To show answer for question number: " + currentIndex);
+
+    // var showAnswer = this.state.cards[currentIndex].answer;
+    // console.log("The answer is: " + showAnswer);
+
+    this.setState({showAnswer: true});
   }
 
   render() {
+    var currentIndex = this.state.index;
+    console.log("Current Index: " + currentIndex);
 
-    // calling cx sets all the styles on the element in the display variable
-    const display = cx(
-      styles.myclass, // styles that never change
-      { // dynamic styles
-        [styles.clicked]: this.state.clicked // make the key the style name, and the value the dynamic boolean
-      }
-    )
+    if(this.props.startCard === false) {
+        var showQuestion = styles.hideQuestion;
+    } else {
+        var showQuestion = styles.showQuestion;
+    }
+
+    if(this.state.showAnswer === true) {
+        var showAnswer = styles.showAnswer;
+    } else {
+        var showAnswer = styles.hideAnswer;
+    }
 
     return (
-      <div>
-        <p className={display}>yay</p>
-        <button onClick={()=>{this.clickHandler()}}>click to change</button>
-        <input className={styles.name} />
+        <div>
+          <div className={showQuestion}>
+            <p>Question: {this.state.cards[currentIndex].question}</p>
+            <button onClick={() => this.clickNext()}>Next Question</button>
+            <button onClick={() => this.clickAnswer()}>Show Answer</button>
+          </div>
+          <p className={showAnswer}>Answer: {this.state.cards[currentIndex].answer}</p>
       </div>
     );
   }
