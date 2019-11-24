97018df3fefdbf4277da985e11c37ed27541ac20
wongyijin8183 <wongyijin8183@gmail.com>
2019-11-25 01:47:50 +0800

    MVP done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {resolve} = require('path');
 const express = require('express');
 
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 process.env.PORT = process.env.PORT || 3000;
