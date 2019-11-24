93091b2059848259752efc94a65e56cb8f99aa6a
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-24 16:54:07 +0800

    Added to pass in start value to form

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,14 +4,42 @@ import { hot } from 'react-hot-loader';
 import Form from './components/form/form';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Form />
-        Welcome.
-      </div>
-    );
-  }
+
+    constructor(){
+        super();
+        this.state = {
+            start: false
+        };
+    }
+
+    clickStart(){
+        // if(this.state.start === true){
+        //     console.log("End button is clicked");
+        //     this.setState({start: false});
+        // } else {
+        //     console.log("Start button is clicked");
+        //     this.setState({start: true});
+        // }
+        this.setState({start: !this.state.start});
+    }
+
+    render() {
+
+        if(this.state.start === true) {
+            var showEndBtn = "End";
+        } else {
+            var showEndBtn = "Start";
+        }
+
+        return (
+            <div>
+                <h1>Welcome</h1>
+                <hr/>
+                <button onClick={() => {this.clickStart()}}>{showEndBtn}</button>
+                <Form startCard={this.state.start} />
+            </div>
+        );
+    }
 }
 
 export default hot(module)(App);
