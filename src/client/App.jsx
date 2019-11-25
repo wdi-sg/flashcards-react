786a227062409f423d50819439c9ab2a6cab830b
EugeneTan9 <tyx.eugene@gmail.com>
2019-11-25 09:25:55 +0800

    Managed to get the questions and answers to show up but still can't use it as a component

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,14 +4,62 @@ import { hot } from 'react-hot-loader';
 import Form from './components/form/form';
 
 class App extends React.Component {
+
+    constructor(){
+    super();
+
+    this.state = {
+        words:"",
+        counter: 0
+    };
+
+    let i = -1;
+
+    const cards = [
+    {
+        question: 'What is this',
+        answer: 'whatever'
+    },
+    {
+        question: 'capital of Malaysia',
+        answer: 'Kuala Lumpur'
+    },
+    {
+        question: 'Kilometers in a mile',
+        answer: 1.6
+    },
+    ];
+  }
+
+
+
+
+
+  nextHandler(){
+    this.i ++;
+    console.log("i :", i);
+    this.setState({counter: i});
+    this.setState({words:cards[i]});
+    console.log(this.state.words);
+  }
+
   render() {
     return (
       <div>
-        <Form />
-        Welcome.
+        <Form nextHandler= {this.nextHandler}/>
+        <h2>{this.state.counter + 1}</h2><br/>
+        <div className="qn">
+        <h2>{this.state.words.question}</h2><br/>
+        <h2>{this.state.words.answer}</h2>
+        </div>
+        <button onClick={()=>{this.nextHandler()}}>NEXT</button>
+
       </div>
     );
   }
 }
+
+
+
 
 export default hot(module)(App);
