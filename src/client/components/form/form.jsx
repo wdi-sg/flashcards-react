be3a6408bc6d31da8e2357d9a1a6bd4886ee3297
TanWeeKiat <tanwee.kiat@live.com.sg>
2019-11-25 09:48:03 +0800

    mvp

--- src/client/components/form/form.jsx
+++ src/client/components/form/form.jsx
@@ -4,38 +4,90 @@ import classnames from 'classnames';
 
 import styles from './style.scss';
 
-const cx = classnames.bind(styles)
+const cx = classnames.bind(styles);
+
+const cards = [
+  {
+    question: '2+2',
+    answer: 4
+  },
+  {
+    question: 'capital of Malaysia',
+    answer: 'Kuala Lumpur'
+  },
+  {
+    question: 'Kilometers in a mile',
+    answer: 1.6
+  }
+];
 
 class Form extends React.Component {
-
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      clicked:false
+      clicked: false,
+      cardNo: 0
     };
   }
 
-  clickHandler(){
+  clickLeft() {
+    let { cardNo } = this.state;
+    if (cardNo > 0)
+      this.setState({ clicked: !this.state.clicked, cardNo: cardNo - 1 });
+  }
 
-    this.setState({clicked:!this.state.clicked})
+  clickRight() {
+    let { cardNo } = this.state;
+    if (cardNo < cards.length - 1)
+      this.setState({ clicked: !this.state.clicked, cardNo: cardNo + 1 });
+  }
+
+  flip() {
+    let { clicked } = this.state;
+    this.setState({ clicked: !this.state.clicked });
   }
 
   render() {
-
     // calling cx sets all the styles on the element in the display variable
     const display = cx(
       styles.myclass, // styles that never change
-      { // dynamic styles
+      {
+        // dynamic styles
         [styles.clicked]: this.state.clicked // make the key the style name, and the value the dynamic boolean
       }
-    )
+    );
+
+    const { cardNo, clicked } = this.state;
 
     return (
       <div>
-        <p className={display}>yay</p>
-        <button onClick={()=>{this.clickHandler()}}>click to change</button>
-        <input className={styles.name} />
+        <p className={display}>
+          {clicked === false ? cards[cardNo].question : cards[cardNo].answer}
+        </p>
+        <button
+          onClick={() => {
+            this.clickleft();
+          }}
+        >
+          left
+        </button>
+
+        <button
+          onClick={() => {
+            this.flip();
+          }}
+        >
+          flip card!
+        </button>
+
+        <button
+          onClick={() => {
+            this.clickRight();
+          }}
+        >
+          right
+        </button>
       </div>
     );
   }
